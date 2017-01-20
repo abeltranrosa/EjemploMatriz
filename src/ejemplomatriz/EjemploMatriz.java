de7053fbe7710d1d9f6c6f75a911aa30fb04b57e
antonio <antonio@biruseto>
2017-01-20 11:28:28 +0100

    main matriz

---
+++ src/ejemplomatriz/EjemploMatriz.java
@@ -0,0 +1,15 @@
+
+package ejemplomatriz;
+
+
+public class EjemploMatriz {
+
+    
+    public static void main(String[] args) {
+        MetodosMatriz obx = new MetodosMatriz();
+        obx.cargarMatriz();
+        obx.amosarMatriz();
+        
+    }
+    
+}
